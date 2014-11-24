04e2ea91785a6f38e5328f66ec9e3fb7c51df6d3
Jerry Anselmi <janselmi@mppi.gob.ve>
2014-11-24 18:20:56 -0430

    commit de respaldo

--- sphinxdoc/helpers/files.py
+++ sphinxdoc/helpers/files.py
@@ -1,12 +1,83 @@
 # -*- coding: utf-8 -*-
-# 
-def get_files(path,ext='py'):
+
+from __future__ import unicode_literals
+import os, shutil
+from terminal_text_color import TextColor,AlertTextColor
+
+def get_files(ruta_origen,ext='py'):
     """Esta función nos permite obtener los archivos de una determinada extención dentro de una ruta"""
-    import os
     cfiles = []
-    for root, dirs, files in os.walk(path):
+    for root, dirs, files in os.walk(ruta_origen):
         for file in files:
             if file.endswith('.'+ext):
                 cfiles.append(os.path.join(root, file))
 
     return cfiles
+
+def zip_dir(ruta_origen,ruta_destino):
+    """Esta función permite crear un archivo .zip de una carpeta"""
+    try:
+        shutil.make_archive(ruta_destino, 'zip', ruta_origen)
+    except EnvironmentError:
+        return False
+    else:
+        return True    
+
+def abspath(ruta_origen):
+    """Esta función permite obtener la ruta absoluta de un path"""
+    return os.path.abspath(ruta_origen)
+
+def basename(ruta_origen):
+    """Esta función permite obtener el nombre base de la ruta espesificada"""
+    return os.path.basename(ruta_origen)
+
+def move(ruta_origen,ruta_destino):
+    """Esta función permite mover un archivo o directorio a otra locación"""
+    try:
+        shutil.move(ruta_origen,ruta_destino)
+    except EnvironmentError:
+        return False
+    else:
+        return True
+
+def backup_files(ruta_origen):
+    
+    respuesta = False
+    archivos = get_files(ruta_origen)
+    zipname = './'+basename(ruta_origen)
+    zipfilename = zipname+'.zip'
+    backupfilename = abspath(ruta_origen)+'/'+zipfilename
+    tc = TextColor()
+    atc= AlertTextColor()
+
+    print tc.default_cyan("Creando backup de del proyecto...")
+
+    if zip_dir(ruta_origen,zipname):
+
+        if move(zipfilename,backupfilename):
+
+            respuesta = True
+
+            print tc.default_green("Backup creado..")
+
+        else:
+
+            atc.error("No se Pudo mover el Archivo de Respaldos .zip a la carpeta destino")
+
+    else:
+
+        atc.error("No se Pudo crear el Archivo de Respaldos .zip")
+
+    return respuesta
+
+
+def pyment_files(ruta_origen):
+    """Esta función permite estructurar una lista de archivos obtimos para utilizar los comandos de Pyment"""
+    files = []
+    for f in get_files(ruta_origen):
+        files.append(dict(original=f,patch=f+".patch"))
+
+    return files
+
+def apply_patch(ruta_origen):
+    os.system("patch -s -p1 < "+ruta_origen+" && rm "+ruta_origen)
--- sphinxdoc/sphinxdoc.py
+++ sphinxdoc/sphinxdoc.py
@@ -1,5 +1,41 @@
 # -*- coding: utf-8 -*-
 
-from helpers.files import get_files
+from helpers.files import get_files, zip_dir, abspath,basename,move,pyment_files, backup_files,apply_patch
+from pyment import PyComment
+from terminal_text_color import TextColor,AlertTextColor
+import os
 
-print get_files('../example-doc-project')
+proyecto = '../example-doc-project'
+
+tc = TextColor()
+
+if backup_files(proyecto):
+
+    archivos = pyment_files(proyecto)
+
+    tc.default_cyan("Creando documentación..")
+
+    for archivo in archivos:
+        c = PyComment(archivo['original'])
+        c.proceed()
+        c.diff_to_file(archivo['patch'])
+        apply_patch(archivo['patch'])
+
+
+
+
+
+# archivos = pyment_files(proyecto)
+
+# zipname = './'+basename(proyecto)
+
+# zipfilename = zipname+'.zip'
+
+
+
+# if zip_dir(proyecto,zipname):
+#     if move(zipfilename,abspath(proyecto)+'/'+zipfilename):
+#         for archivo in archivos:
+#             c = PyComment(archivo['original'])
+#             c.proceed()
+#             c.diff_to_file(archivo['patch'])
