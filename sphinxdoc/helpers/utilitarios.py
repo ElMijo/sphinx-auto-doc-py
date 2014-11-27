988c91361a9fb38cf5d706fc4255e7d6d8fd6f1e
Jerry Anselmi <janselmi@mppi.gob.ve>
2014-11-27 18:09:59 -0430

    commit de respaldo..

--- sphinxdoc/helpers/utilitarios.py
+++ sphinxdoc/helpers/utilitarios.py
@@ -4,71 +4,71 @@ from __future__ import unicode_literals
 import os, shutil
 from terminal_text_color import TextColor,AlertTextColor
 
-def get_files(ruta_origen,ext='py'):
-    """Esta función nos permite obtener los archivos de una determinada extención dentro de una ruta"""
-    cfiles = []
-    for root, dirs, files in os.walk(ruta_origen):
-        for file in files:
-            if file.endswith('.'+ext):
-                cfiles.append(os.path.join(root, file))
+# def get_files(ruta_origen,ext='py'):
+#     """Esta función nos permite obtener los archivos de una determinada extención dentro de una ruta"""
+#     cfiles = []
+#     for root, dirs, files in os.walk(ruta_origen):
+#         for file in files:
+#             if file.endswith('.'+ext):
+#                 cfiles.append(os.path.join(root, file))
 
-    return cfiles
+#     return cfiles
 
-def zip_dir(ruta_origen,ruta_destino):
-    """Esta función permite crear un archivo .zip de una carpeta"""
-    try:
-        shutil.make_archive(ruta_destino, 'zip', ruta_origen)
-    except EnvironmentError:
-        return False
-    else:
-        return True    
+# def zip_dir(ruta_origen,ruta_destino):
+#     """Esta función permite crear un archivo .zip de una carpeta"""
+#     try:
+#         shutil.make_archive(ruta_destino, 'zip', ruta_origen)
+#     except EnvironmentError:
+#         return False
+#     else:
+#         return True    
 
-def abspath(ruta_origen):
-    """Esta función permite obtener la ruta absoluta de un path"""
-    return os.path.abspath(ruta_origen)
+# def abspath(ruta_origen):
+#     """Esta función permite obtener la ruta absoluta de un path"""
+#     return os.path.abspath(ruta_origen)
 
-def basename(ruta_origen):
-    """Esta función permite obtener el nombre base de la ruta espesificada"""
-    return os.path.basename(ruta_origen)
+# def basename(ruta_origen):
+#     """Esta función permite obtener el nombre base de la ruta espesificada"""
+#     return os.path.basename(ruta_origen)
 
-def move(ruta_origen,ruta_destino):
-    """Esta función permite mover un archivo o directorio a otra locación"""
-    try:
-        shutil.move(ruta_origen,ruta_destino)
-    except EnvironmentError:
-        return False
-    else:
-        return True
+# def move(ruta_origen,ruta_destino):
+#     """Esta función permite mover un archivo o directorio a otra locación"""
+#     try:
+#         shutil.move(ruta_origen,ruta_destino)
+#     except EnvironmentError:
+#         return False
+#     else:
+#         return True
 
-def backup_files(ruta_origen):
-    """Esta función nos permite crear aun archivo zip de respaldo del proyecto donde se esta creando la ddocumentación"""
-    respuesta = False
-    archivos = get_files(ruta_origen)
-    zipname = './'+basename(ruta_origen)
-    zipfilename = zipname+'.zip'
-    backupfilename = abspath(ruta_origen)+'/'+zipfilename
-    tc = TextColor()
-    atc= AlertTextColor()
+# def backup_files(ruta_origen):
+#     """Esta función nos permite crear aun archivo zip de respaldo del proyecto donde se esta creando la ddocumentación"""
+#     respuesta = False
+#     archivos = get_files(ruta_origen)
+#     zipname = './'+basename(ruta_origen)
+#     zipfilename = zipname+'.zip'
+#     backupfilename = abspath(ruta_origen)+'/'+zipfilename
+#     tc = TextColor()
+#     atc= AlertTextColor()
 
-    print tc.default_cyan("Creando backup de del proyecto...")
+#     print tc.default_cyan("Creando backup de del proyecto...")
 
-    if zip_dir(ruta_origen,zipname):
+#     if zip_dir(ruta_origen,zipname):
 
-        if move(zipfilename,backupfilename):
+#         if move(zipfilename,backupfilename):
 
-            respuesta = True
+#             respuesta = True
 
-            print tc.default_green("Backup creado..")
+#             print tc.default_green("Backup creado..")
 
-        else:
+#         else:
 
-            atc.error("No se Pudo mover el Archivo de Respaldos .zip a la carpeta destino")
+#             atc.error("No se Pudo mover el Archivo de Respaldos .zip a la carpeta destino")
 
-    else:
+#     else:
 
-        atc.error("No se Pudo crear el Archivo de Respaldos .zip")
+#         atc.error("No se Pudo crear el Archivo de Respaldos .zip")
 
-    return respuesta
+#     return respuesta
 
 def pyment_files(ruta_origen):
     """Esta función permite estructurar una lista de archivos obtimos para utilizar los comandos de Pyment"""
@@ -83,9 +83,16 @@ def apply_patch(archivo):
     os.system("patch "+archivo['original']+" "+archivo['patch'])
     os.system("rm "+archivo['patch'])
 
-def sphinx_quickstart(ruta_origen):
-    """Esta función ejecuta el comando sphinx-quickstart de Sphinx para crear la documentación por primera vez"""
-    os.system("sphinx-quickstart "+ruta_origen+"/doc")
+# def sphinx_quickstart(ruta_origen):
+#     """Esta función ejecuta el comando sphinx-quickstart de Sphinx para crear la documentación por primera vez"""
+#     os.system("sphinx-quickstart "+ruta_origen+"/docs")
+
+# def sphinx_apidoc(ruta_origen,package):
+#     """Esta función ejecuta el comando sphinx-apidoc de Sphinx para crear la documentación del API"""
+#     ruta_package = ruta_origen+'/'+package
+#     ruta_docs = ruta_origen+'/docs'
+#     os.system('sphinx-apidoc -F -o '+ruta_docs+' '+ruta_package)
+
 
 def replace_line(archivo, linea, texto):
     lines = open(archivo, 'r').readlines()
@@ -99,10 +106,12 @@ def remove_end_black_line(archivo):
     open('copia.rst', "w").write(text)
 
 
-def add_packge(ruta_origen):
-    texto = 'sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))'
-    archivo = ruta_origen+"/doc/conf.py"
+def add_package(ruta_origen):
+    texto = 'sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))\n'
+    archivo = ruta_origen+"/docs/conf.py"
     replace_line(archivo, 17, texto)
+    add_extensions(archivo)
+    remove_epub_options(archivo)
 
 def add_api_doc(ruta_origen): 
     archivo = ruta_origen+'/index.rst'
@@ -114,34 +123,71 @@ def add_api_doc(ruta_origen):
             break
 
     if line:
-        replace_line(archivo, line, '   api/modules\n')
+        replace_line(archivo, line, '   modules\n\n')
 
-def generate_api(ruta_origen,packge):
-    apifolder = ruta_origen+'/doc/api'
-    crerate_api_folder(apifolder)
-    os.system("python ./sphinxdoc/script/generate_modules -d "+apifolder+" -s rst "+packge)
-    add_api_doc(ruta_origen+'/doc')
-    for archivo in get_files(apifolder):
+def generate_api(ruta_origen,package):
+    apifolder = abspath(ruta_origen+'/docs')
+    packfolder = abspath(ruta_origen+'/'+package)
+    #crerate_api_folder(apifolder)
+    os.system("python ./sphinxdoc/script/generate_modules -d "+apifolder+" -s rst -f "+packfolder)
+    add_api_doc(apifolder)
+    for archivo in get_files(apifolder,ext='rst'):
         remove_end_black_line(archivo)
 
-def crerate_api_folder(directorio):
-    if not os.path.exists(directorio):
-        os.makedirs(directorio)
+# def crerate_api_folder(directorio):
+#     if not os.path.exists(directorio):
+#         os.makedirs(directorio)
 
-def generate_html_doc(ruta_origen):
-    pwd = os.getcwd()
-    docfolder = ruta_origen+'/doc'
-    os.system("make -C %s html" % docfolder)
+# def generate_html_doc(ruta_origen):
+#     pwd = os.getcwd()
+#     docfolder = ruta_origen+'/docs'
+#     os.system("make -C %s html" % docfolder)
 
-def get_principal_package(ruta_origen):
-    import re
-    from setuptools import find_packages
-    principal_package = None
-    patron = re.re.compile('[.]')
-    list_packages = find_packages(where=ruta_origen,exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
-    for item in list_packages:
-        if  patron.search(pack) is None:
-            principal_package = item
+# def get_principal_package(ruta_origen):
+#     import re
+#     from setuptools import find_packages
+#     principal_package = None
+#     patron = re.compile('[.]')
+#     list_packages = find_packages(where=ruta_origen,exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+#     for item in list_packages:
+#         if  patron.search(item) is None:
+#             principal_package = item
+#             break
+
+#     return principal_package
+
+def add_extensions(archivo):
+    from string import find
+    lines = open(archivo,"r").readlines()
+    ext = [
+        "'sphinx.ext.autodoc',\n",
+        "'sphinx.ext.doctest',\n",
+        "'sphinx.ext.mathjax',\n",
+        "'sphinx.ext.viewcode',\n"
+    ]
+    inx_init = 0
+    inx_end = 0
+    for inx,val in enumerate(lines):
+        if  find(val,'extensions = [')>-1:
+            inx_init = inx
+            continue
+        elif find(val,']') > -1:
+            inx_end = inx
             break
 
-    return principal_package
+    lines = lines[:inx_init+1]+ext+lines[inx_end:]
+    out = open(archivo, 'w')
+    out.writelines(lines)
+    out.close()
+
+def remove_epub_options(archivo):
+    from string import find
+    lines = open(archivo,"r").readlines()
+    for inx,val in enumerate(lines):
+        if  find(val,'Options for Epub output')>-1:
+            lines = lines[:inx]
+            break
+
+    out = open(archivo, 'w')
+    out.writelines(lines)
+    out.close()
--- sphinxdoc/sphinxdoc.py
+++ sphinxdoc/sphinxdoc.py
@@ -1,36 +1,46 @@
 # -*- coding: utf-8 -*-
 
-from helpers.utilitarios import pyment_files, backup_files,apply_patch, sphinx_quickstart, add_packge, generate_api,crerate_api_folder,generate_html_doc,get_principal_package
-from pyment import PyComment
-from terminal_text_color import TextColor,AlertTextColor
-import os, sys
+# class ClassName(object):
+#     """docstring for ClassName"""
+#     def __init__(self, arg):
+#         super(ClassName, self).__init__()
+#         self.arg = arg
+#         
+from terminal_text_color import TextColor,AlertTextColor  
 
-proyecto = '../example-doc-project'
-package = get_principal_package(proyecto)
-#+'/trianglelib'
+class SphinxDoc(object):
+    def __init__(self, ruta_proyecto):
+        self.tc = TextColor()
+        self.atc = AlertTextColor()
 
-tc = TextColor()
+# from helpers.utilitarios import pyment_files, backup_files,apply_patch, sphinx_quickstart, generate_api,crerate_api_folder,generate_html_doc,get_principal_package,sphinx_apidoc,add_package
+# from pyment import PyComment
+# from terminal_text_color import TextColor,AlertTextColor
+# import os, sys
 
-if backup_files(proyecto):
+# proyecto = '../example-doc-project'
+# package = get_principal_package(proyecto)
+# #+'/trianglelib'
 
-    archivos = pyment_files(proyecto)
+# tc = TextColor()
 
-    print tc.default_cyan("Convirtiendo docstring en reStructuredText..")
+# if backup_files(proyecto):
 
-    for archivo in archivos:
-        c = PyComment(archivo['original'])
-        c.proceed()
-        c.diff_to_file(archivo['patch'])
-        apply_patch(archivo)
+#     archivos = pyment_files(proyecto)
 
-    print tc.default_cyan("Creando documentación..")
-    print ""
-    print tc.bold_yellow("Esto es una secuencia interactiva responda las preguntas segun la configuración que desee")
-    print ""  
-    sphinx_quickstart(proyecto)
-    print tc.default_cyan("Agregando el proyecto a la ayuda..")
-    add_package(proyecto)
-    print tc.bold_cyan("Generando la documentación del API...")
-    generate_api(proyecto,package)
-    print tc.bold_yellow("Generando la documentación HTML")
-    generate_html_doc(proyecto)
+#     print tc.default_cyan("Convirtiendo docstring en reStructuredText..")
+
+#     for archivo in archivos:
+#         c = PyComment(archivo['original'])
+#         c.proceed()
+#         c.diff_to_file(archivo['patch'])
+#         apply_patch(archivo)
+
+#     print tc.default_cyan("Creando documentación..")
+#     sphinx_apidoc(proyecto,package)
+#     print tc.default_cyan("Agregando el proyecto a la ayuda..")
+#     add_package(proyecto)
+#     print tc.bold_cyan("Generando la documentación del API...")
+#     generate_api(proyecto,package)
+#     print tc.bold_yellow("Generando la documentación HTML")
+#     generate_html_doc(proyecto)
